6c9eac2fb40274c5c531cc1bb409231ce1c947f9
mahakagarwal248 <mahak.agarwal@techchefz.com>
2022-12-31 19:01:55 +0530

    new method of json conversion

--- controllers/csvToJson.js
+++ controllers/csvToJson.js
@@ -1,18 +1,37 @@
-import excel from 'exceljs'
+import excel from "exceljs";
 const csvToJson = async (req, res) => {
   const filePath = req.file.path;
   const workbook = new excel.Workbook();
   await workbook.xlsx.readFile(filePath);
+  let jsonData = [];
+
   try {
-    const book = [];
-    workbook.eachSheet((sheet) => {
-      const sheets = [];
-      sheet.eachRow((row) => {
-        sheets.push(row.values);
+    // const book = [];
+    // workbook.eachSheet((sheet) => {
+    //   const sheets = [];
+    //   sheet.eachRow((row) => {
+    //     sheets.push(row.values);
+    //   });
+    //   book.push(sheets);
+    // });
+    // return res.status(200).json({ headers: book[0][4], data: book[0] });
+
+    workbook.worksheets.forEach(function (sheet) {
+      // read first row as data keys
+      let firstRow = sheet.getRow(5);
+      if (!firstRow.cellCount) return;
+      let keys = firstRow.values;
+      sheet.eachRow((row, rowNumber) => {
+        if (rowNumber == 5) return;
+        let values = row.values;
+        let obj = {};
+        for (let i = 1; i < keys.length; i++) {
+          obj[keys[i]] = values[i];
+        }
+        jsonData.push(obj);
       });
-      book.push(sheets);
     });
-    return res.status(200).json({ headers: book[0][4], data: book[0][8] });
+    return res.status(200).json(jsonData)
   } catch (error) {
     console.log(error);
     return res.status(400).json({ message: error.message });
